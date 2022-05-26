1de3ae1b41352468bfaa4936a5be5a60fee0d7ae
piyushsud <piyushsud@gmail.com>
2022-05-25 23:17:11 -0700

    wrote code for frame conversion

--- camera_calibration/determine_camera_extrinsics_large_checkerboard.py
+++ camera_calibration/determine_camera_extrinsics_large_checkerboard.py
@@ -2,8 +2,8 @@ import numpy as np
 import cv2
 
 # Define the dimensions of checkerboard
-CHECKERBOARD = (9, 6)
-# CHECKERBOARD = (4, 7)
+# CHECKERBOARD = (9, 6)
+CHECKERBOARD = (4, 7)
 
 # stop the iteration when specified
 # accuracy, epsilon, is reached or
@@ -12,27 +12,27 @@ criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # 3D points real world coordinates
 objectp3d = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
-for i in range(6):
-    for j in range(9):
-        objectp3d[0][i*9 + j] = np.array([i, 8 - j, 0])
+# for i in range(6):
+#     for j in range(9):
+#         objectp3d[0][i*9 + j] = np.array([i, 8 - j, 0])
 
-# for i in range(7):
-#     for j in range(4):
-#         objectp3d[0][i*4 + j] = np.array([6 - i, j, 0])
+for i in range(7):
+    for j in range(4):
+        objectp3d[0][i*4 + j] = np.array([6 - i, j, 0])
 
-# camera_matrix = np.array([[591.48522865,   0.,         322.52619954],
-#  [  0.,         591.9638662,  257.7496392 ],
-#  [  0.,           0.,           1.        ]])
+camera_matrix = np.array([[591.48522865,   0.,         322.52619954],
+ [  0.,         591.9638662,  257.7496392 ],
+ [  0.,           0.,           1.        ]])
+
+distortion = np.array([[ 0.00252699,  0.50539056,  0.00564689,  0.00742319, -1.62753842]])
+
+# camera_matrix = np.array([[1.15090805e+03, 0.00000000e+00, 6.85823395e+02],
+#  [0.00000000e+00, 1.14906092e+03, 4.79730926e+02],
+#  [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
 #
-# distortion = np.array([[ 0.00252699,  0.50539056,  0.00564689,  0.00742319, -1.62753842]])
+# distortion = np.array([[ 0.06571678, -0.06531794, -0.00267922, -0.00469088, -0.05419306]])
 
-camera_matrix = np.array([[1.15090805e+03, 0.00000000e+00, 6.85823395e+02],
- [0.00000000e+00, 1.14906092e+03, 4.79730926e+02],
- [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
-
-distortion = np.array([[ 0.06571678, -0.06531794, -0.00267922, -0.00469088, -0.05419306]])
-
-image = cv2.imread("C:/Users/piyus/Robot_Arm_Ping_Pong/camera_calibration/black_camera_images/checkerboard_1.png")
+image = cv2.imread("C:/Users/piyus/Robot_Arm_Ping_Pong/camera_calibration/realsense_images/checkerboard_upright.png")
 
 grayColor = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -42,6 +42,9 @@ ret, corners = cv2.findChessboardCorners(
     + cv2.CALIB_CB_FAST_CHECK +
     cv2.CALIB_CB_NORMALIZE_IMAGE)
 
+image = cv2.circle(image,  (279, 207),  10, (0,0,255), -1)
+cv2.imshow("image", image)
+cv2.waitKey(0)
 
 if ret is True:
     corners2 = cv2.cornerSubPix(
--- frameConverter.py
+++ frameConverter.py
@@ -1,8 +1,5 @@
-import numpy as np
-
 import cv2
 import numpy as np
-
 
 # checkerboard upright frame = frame A
 # checkerboard on table frame = frame B
@@ -21,69 +18,33 @@ T_be = np.array([[0.99965129, -0.01992817, -0.01732549, 3.1662789],
                  [0.01807638,  0.03813822,  0.99910896, 23.29714278],
                  [0, 0, 0, 1]])
 
+T_ca = np.array(
+    [
+        [-1, 0, 0, 0.4534875],
+        [0, 0, -1, -0.2311625],
+        [0, -1, 0, 0.2],
+        [0, 0, 0, 1]
+    ]
+)
+
+T_cb = np.array(
+    [
+        [1, 0, 0, 0.4525],
+        [0, -1, 0, 0.0545],
+        [0, 0, -1, 0],
+        [0, 0, 0, 1]
+    ]
+)
 
 
-print(T_ad)
-print(T_be)
+# T_ab * p_b changes the reference frame of p from b to a.
+p_d = np.array([0, 0, 0, 1])
+p_a = np.matmul(T_ad, p_d)
+p_c1 = np.matmul(T_ca, p_a)
 
+p_e = np.array([0, 0, 0, 1])
+p_b = np.matmul(T_be, p_e)
+p_c2 = np.matmul(T_cb, p_b)
 
-
-# old code
-
-
-
-
-IMG_HEIGHT = 480
-IMG_WIDTH = 640
-FX = 605.00622559  # focal length in x-direction in pixels
-FY = 604.44366455  # focal length in y-direction in pixels
-CX = 315.97814941  # optical center x-coordinate in pixels
-CY = 246.10261536  # optical center y-coordinate in pixels
-
-# realsense to world transformation matrix
-
-Tcw = np.array([
-    [0.99945658, -0.02437214, 0.02219342, -0.05714963],
-    [-0.0202, 0.07919504, 0.99665446, -1.17350852],
-    [-0.02604821, -0.99656116, 0.07865968, 0.10698736],
-    [0., 0., 0., 1.]
-])
-
-# camera 2 to world transformation matrix
-Tcw2 = np.array([
-    [-0.0806599138, 0.0857617880, -0.993045263, 1.39795874],
-    [0.996714447, -0.000424886245, -0.0809946368, 0.142971742],
-    [-0.00736817614, -0.996315580, -0.0854457418, 0.648028519],
-    [0, 0, 0, 1]
-])
-
-class FrameConverter:
-    def __init__(self):
-        pass
-
-    def image_to_camera_frame(self, depth_img, r, c):
-        depth = depth_img[c, r]/1000
-        x = (c - CX)*depth/FX
-        y = (r - CY)*depth/FY
-        z = depth
-        return x, y, z
-
-    # see conventions.txt for frame orientations
-    def camera_to_world_frame(self, x, y, z, camera):
-        camera_pt = np.array([x, y, z, 1])
-
-        if camera == "realsense":
-            world_pt = np.matmul(Tcw, camera_pt)
-        else:
-            world_pt = np.matmul(Tcw2, camera_pt)
-
-        return world_pt[0], world_pt[1], world_pt[2]
-
-    def world_to_robot_frame(self, x_world, y_world, z_world):
-
-        x_robot = -x_world
-        y_robot = -y_world
-        z_robot = z_world
-
-        return x_robot, y_robot, z_robot
-
+print(p_c1)
+print(p_c2)
