b0034a9b5643f20d8939a968582d1bd178c1a5cf
piyushsud <piyushsud@gmail.com>
2022-06-12 22:03:34 -0700

    added new transformation matrix for realsense mount, but blob detector doesn't work with non-white background

--- camera_calibration/determine_camera_extrinsics_large_checkerboard.py
+++ camera_calibration/determine_camera_extrinsics_large_checkerboard.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 # Define the dimensions of checkerboard
-CHECKERBOARD = (9, 6)
+CHECKERBOARD = (6, 9)
 # CHECKERBOARD = (4, 7)
 
 # stop the iteration when specified
@@ -12,29 +12,32 @@ criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # 3D points real world coordinates
 objectp3d = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
-for i in range(6):
-    for j in range(9):
-        objectp3d[0][i*9 + j] = np.array([i, 8 - j, 0])
+for i in range(9):
+    for j in range(6):
+        objectp3d[0][i*6 + j] = np.array([8 - i, j, 0])
 
 # for i in range(7):
 #     for j in range(4):
 #         objectp3d[0][i*4 + j] = np.array([6 - i, j, 0])
 
-# camera_matrix = np.array([[591.48522865,   0.,         322.52619954],
-#  [  0.,         591.9638662,  257.7496392 ],
-#  [  0.,           0.,           1.        ]])
+camera_matrix = np.array([[591.48522865,   0.,         322.52619954],
+ [  0.,         591.9638662,  257.7496392 ],
+ [  0.,           0.,           1.        ]])
+
+distortion = np.array([[ 0.00252699,  0.50539056,  0.00564689,  0.00742319, -1.62753842]])
+
+# camera_matrix = np.array([[1.15090805e+03, 0.00000000e+00, 6.85823395e+02],
+#  [0.00000000e+00, 1.14906092e+03, 4.79730926e+02],
+#  [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
 #
-# distortion = np.array([[ 0.00252699,  0.50539056,  0.00564689,  0.00742319, -1.62753842]])
+# distortion = np.array([[ 0.06571678, -0.06531794, -0.00267922, -0.00469088, -0.05419306]])
 
-camera_matrix = np.array([[1.15090805e+03, 0.00000000e+00, 6.85823395e+02],
- [0.00000000e+00, 1.14906092e+03, 4.79730926e+02],
- [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
+image = cv2.imread("C:/Users/piyus/Robot_Arm_Ping_Pong/camera_calibration/realsense_images/checkerboard_upright.png")
+image_copy = np.copy(image)
 
-distortion = np.array([[ 0.06571678, -0.06531794, -0.00267922, -0.00469088, -0.05419306]])
-
-image = cv2.imread("C:/Users/piyus/Robot_Arm_Ping_Pong/camera_calibration/black_camera_images/checkerboard_1.png")
-
+image = image[200:430, 150:460]
 grayColor = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+grayColorFull = cv2.cvtColor(image_copy, cv2.COLOR_BGR2GRAY)
 
 ret, corners = cv2.findChessboardCorners(
     grayColor, CHECKERBOARD,
@@ -42,14 +45,25 @@ ret, corners = cv2.findChessboardCorners(
     + cv2.CALIB_CB_FAST_CHECK +
     cv2.CALIB_CB_NORMALIZE_IMAGE)
 
-image = cv2.circle(image,  (279, 207),  10, (0,0,255), -1)
+
+corners_full = []
+for corner in corners:
+    # print(corner[])
+    corners_full.append([[corner[0][0] + 150, corner[0][1] + 200]])
+
+corners_full = np.array(corners_full, dtype=np.float32)
+# print(corners_full.dtype)
+image = cv2.circle(image_copy,  (216, 239),  10, (0, 0, 255), -1)
 cv2.imshow("image", image)
 cv2.waitKey(0)
 
 if ret is True:
     corners2 = cv2.cornerSubPix(
-        grayColor, corners, (11, 11), (-1, -1), criteria)
+        grayColorFull, corners_full, (11, 11), (-1, -1), criteria)
 
+    # image_copy = cv2.drawChessboardCorners(image_copy, CHECKERBOARD, corners2, True)
+    # cv2.imshow("img", image_copy)
+    # cv2.waitKey(0)
     # MAKE SURE ORDER OF 2D POINTS MATCHES ORDER OF 3D POINTS
     print(corners2, objectp3d)
 
--- misc/realsense_take_picture.py
+++ misc/realsense_take_picture.py
@@ -1,111 +1,16 @@
-## License: Apache 2.0. See LICENSE file in root directory.
-## Copyright(c) 2015-2017 Intel Corporation. All Rights Reserved.
-
-###############################################
-##      Open CV and Numpy integration        ##
-###############################################
-
-import pyrealsense2 as rs
-import numpy as np
 import cv2
-import time
 
-TOLERANCE = 0.05
+# intel realsense
+realsense = cv2.VideoCapture(3, cv2.CAP_DSHOW)
+realsense.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+realsense.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-# Configure depth and color streams
-pipeline = rs.pipeline()
+while True:
+    ret, img = realsense.read()
+    cv2.imshow("img", img)
+    cv2.imwrite("C:/Users/piyus/Robot_Arm_Ping_Pong/camera_calibration/realsense_images/checkerboard_upright.png", img)
 
-# cfg = pipeline.start() # Start pipeline and get the configuration it found
-# profile = cfg.get_stream(rs.stream.depth) # Fetch stream profile for depth stream
-# intr = profile.as_video_stream_profile().get_intrinsics() # Downcast to video_stream_profile and fetch intrinsics
-#
-# camera_matrix = np.array([
-#     [intr.fx, 0, intr.ppx],
-#     [0, intr.fy, intr.ppy],
-#     [0, 0, 1]
-# ])
-#
-# print(intr.model, intr.coeffs)
-
-config = rs.config()
-
-# Get device product line for setting a supporting resolution
-pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-pipeline_profile = config.resolve(pipeline_wrapper)
-device = pipeline_profile.get_device()
-device_product_line = str(device.get_info(rs.camera_info.product_line))
-
-found_rgb = False
-for s in device.sensors:
-    if s.get_info(rs.camera_info.name) == 'RGB Camera':
-        found_rgb = True
+    # the 'q' button is set as the
+    # quitting button
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-if not found_rgb:
-    print("The demo requires Depth camera with Color sensor")
-    exit(0)
-
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-
-if device_product_line == 'L500':
-    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
-else:
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-
-# Start streaming
-pipeline.start(config)
-
-# profile = config.get_stream(rs.stream.depth)  # Fetch stream profile for depth stream
-# intr = profile.as_video_stream_profile().get_intrinsics()  # Downcast to video_stream_profile and fetch intrinsics
-# print(intr.ppx)
-
-start_time = time.time()
-picture_number = 0
-busy = False
-
-try:
-    i = 0
-    while True:
-
-        # Wait for a coherent pair of frames: depth and color
-        frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
-        if not depth_frame or not color_frame:
-            continue
-
-        # Convert images to numpy arrays
-        depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
-        i += 1
-
-        # curr_time = time.time()
-        # print(curr_time - start_time)
-        #
-        # if busy is False:
-        #     if abs(curr_time - start_time - 1) < TOLERANCE:
-        #         busy = True
-        #         print(picture_number)
-        #         cv2.imwrite(
-        #             "C:/Users/piyus/Robot_Arm_Ping_Pong/camera_calibration/realsense_images/checkerboard_" +
-        #             str(picture_number) + ".png", color_image)
-        #         picture_number += 1
-        #         start_time = curr_time
-        #
-        # if curr_time - start_time > TOLERANCE:
-        #     busy = False
-
-        # if i == 50:
-        #     cv2.imwrite("C:/Users/piyus/Robot_Arm_Ping_Pong/camera_calibration/realsense_images/checkerboard_upright.png",
-        #                 color_image)
-        #     break
-        cv2.imshow("img", color_image)
-        # the 'q' button is set as the
-        # quitting button you may use any
-        # desired button of your choice
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-finally:
-
-    # Stop streaming
-    pipeline.stop()
